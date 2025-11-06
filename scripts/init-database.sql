817de997a485eb3a97f2f5fee8c14d474c2113a9
sladder128 <bol5er55@gmail.com>
2025-11-06 12:49:38 +0100

    Create init-database.sql

---
+++ scripts/init-database.sql
@@ -0,0 +1,16 @@
+/*
+creates new datawarhouse with 3 schemas
+
+
+*/
+
+USE master;
+CREATE DATABASE baraa;
+USE baraa;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
